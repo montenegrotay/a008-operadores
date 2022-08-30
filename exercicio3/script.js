37cbf1ad5c7661d1464a932c511f228728e68012
Taynara <montenegro.taynara@gmail.com>
2022-08-30 15:33:11 -0300

    Exercícios a008 Operadores

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -8,12 +8,12 @@ console.log(operacaoB)
 /* true, pois 5 é igual a 5, portanto true || true, que devolve true */
 
 
-const operacaoC = !20 > 50;
+const operacaoC = !(20 > 50);
 console.log(operacaoC)
 /* false, pois 20 não é maior que 50, portanto !false, que devolve !false. */
 
 
-const operacaoD = !20 > 50 || !50 > 60;
+const operacaoD = !(20 > 50) || !(50 > 60);
 console.log(operacaoD)
 /* false, pois 20 não é maior que 50 ou 50 não é maior que 60, portanto !false || false, que devolve !false. */
 
